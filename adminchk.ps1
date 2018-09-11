7deb5ef3cc8e863b68e22e1f2a2fc00d448016fb
Vincent Larkin <vincenttheepic@live.com>
2018-09-11 18:31:13 -0500

    Updated

--- adminchk.ps1
+++ adminchk.ps1
@@ -1,6 +1,6 @@
 #Requires -RunAsAdministrator
 
-$strString = "You are ready to debloat Windows 10"
+$strString = "You are ready to use Windows 10"
 write-host $strString
 
 function ftest{
--- main.py
+++ main.py
@@ -5,13 +5,23 @@ import shutil
 
 print("Make Sure This is Running As Admin!")
 print("What would you like to do?")
-print("[A] - Ultra Debloat")
+print("[B] - Block Telemetry")
 print("[C] - Compatibility Check")
+print("[D] - Full Debloat")
+print("Type ""help (letter)"" to see what a command does")
 choice = input("> ")
+
 
 if choice == "C":
 	p = subprocess.Popen(["powershell.exe",
               "scripts\\compatchk.ps1"], 
               stdout=sys.stdout)
 	p.communicate()
+
+if choice == "D":
+	p = subprocess.Popen(["powershell.exe",
+		"scripts\\block-teleetry.ps1"],
+		stdout=sys.stdout)
+	p.communicate()
+
 	
