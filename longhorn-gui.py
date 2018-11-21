e5bfec50ab0c5aa44975d29b1d927a3165753009
Vincent <vinlark@outlook.com>
2018-11-21 09:26:14 -0600

    /

--- longhorn-gui.py
+++ longhorn-gui.py
@@ -1,6 +1,6 @@
 from GUI import Window, Button, RadioGroup, RadioButton, \
     application
 from GUI.Alerts import alert, alert2, alert3, note_alert, stop_alert, \
-    confirm
+    ask, confirm,
 from GUI import Window, View, Image, application
 from GUI.Geometry import offset_rect, rect_sized
