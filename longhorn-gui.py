85d01bcfb5b4a1bab39a2c8d8923084a73c1507c
Vincent L <vinlark@outlook.com>
2018-12-03 21:38:46 -0600

    Made buttons work, added text
    Almost done!

--- longhorn-gui.py
+++ longhorn-gui.py
@@ -1,4 +1,5 @@
-from GUI import Window, Button, RadioGroup, RadioButton, \
+import subprocess, sys, os, shutil, platform
+from GUI import Window, Button, Label, RadioGroup, RadioButton, \
     application
 from GUI.Alerts import alert, alert2, alert3, note_alert, stop_alert, \
     ask, confirm, ask_or_cancel, confirm_or_cancel
@@ -6,21 +7,15 @@ from GUI.Alerts import alert, alert2, alert3, note_alert, stop_alert, \
 
 print("If you see this, running it works.")
 
+def cortana():
+	p = subprocess.Popen(["scripts\\delCortana.exe"])
+	print("Please run [C] twice for it to take full effect.\n")
+
 def defaultapps():
 	p = subprocess.Popen(["powershell.exe",
 		"scripts\\remove-default-apps.ps1"],
 		stdout=sys.stdout)
 	p.communicate()
-
-def telemetry():
-	p = subprocess.Popen(["powershell.exe",
-		"scripts\\block-telemetry.ps1"],
-		stdout=sys.stdout)
-	p.communicate()
-
-def cortana():
-	p = subprocess.Popen(["scripts\\delCortana.exe"])
-	print("Please run [C] twice for it to take full effect.\n")
 
 def defender():
 	p = subprocess.Popen(["powershell.exe",
@@ -28,14 +23,15 @@ def defender():
 		stdout=sys.stdout)
 	p.communicate()
 
-def wininfo():
-	print("----------Windows Info------------")
-	import platform as p; print(p.platform());print(p.processor())
-	print("----------------------------------")
-
-def ondrive():
+def onedrive():
 	p = subprocess.Popen(["powershell.exe",
 		"scripts\\remove-onedrive.ps1"], 
+		stdout=sys.stdout)
+	p.communicate()
+
+def telemetry():
+	p = subprocess.Popen(["powershell.exe",
+		"scripts\\block-telemetry.ps1"],
 		stdout=sys.stdout)
 	p.communicate()
 
@@ -53,6 +49,11 @@ def setup():
 		stdout=sys.stdout)
 	p.communicate()
 
+def wininfo():
+	print("----------Windows Info------------")
+	import platform as p; print(p.platform());print(p.processor())
+	print("----------------------------------")
+
 def winupdate():
 	p = subprocess.Popen(["powershell.exe",
        "scripts\\disable-windows-update.ps1"], 
@@ -63,8 +64,22 @@ def exit():
 	exit()
 
 win = Window(title = "Longhorn v2.0")
-
-
+bt = [
+    Button("Disable Cortana", action = cortana),
+    Button("Remove Default Apps", action = defaultapps),
+    Button("Disable Windows Defender", action = defender),
+    Button("Remove Onedrive", action = onedrive),
+    Button("Block Telemetry", action = telemetry),
+    Button("Disable Intrusive Services", action = services),
+    Button("Setup Longhorn", action = setup),
+    Button("Disable Windows Update", action = winupdate),
+    Button("Exit Longhorn", action = exit),
+]
+lbl = Label("""Longhorn v2.0
+	By VincentXII""", position = (20, 50), width = 200)
+win.place_column(bt, left = 90 + 200, top = 20)
+win.size = (500, 470)
+win.add(lbl)
 
 win.show()
 
